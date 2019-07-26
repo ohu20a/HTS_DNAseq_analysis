65f6a7955cc8634a9e493d3c3a0f2491a0c23412
SichongP <scpeng@ucdavis.edu>
2019-07-26 13:48:52 -0700

    fix bugs in merge_bam.py

--- tools/merge_bam.py
+++ tools/merge_bam.py
@@ -5,13 +5,13 @@
 ### If yes, merge them and output a single bam file
 
 import subprocess
-
 sample = snakemake.wildcards.sample
 files = snakemake.input
-output = snakemake.output
+output = snakemake.output[0]
 threads = snakemake.threads
 print(output)
 if len(files) == 1:
-    subprocess.run(["mv", files[0], output[0]])
+    subprocess.run(["mv", files[0], output])
+    subprocess.run(["touch", files[0]])
 else:
-    subprocess.run(["sambamba", "-t", threads, output[0]].extend(files))
+    subprocess.run(["sambamba", "-t", threads, output].extend(files))
