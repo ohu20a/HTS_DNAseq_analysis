197f4d122b5c5c639e3d6b5aa693d9d2cc1e94c3
SichongP <scpeng@ucdavis.edu>
2019-07-26 10:24:05 -0700

    fix a bug in merge_bam.py script (output[0])

--- tools/merge_bam.py
+++ tools/merge_bam.py
@@ -10,7 +10,8 @@ sample = snakemake.wildcards.sample
 files = snakemake.input
 output = snakemake.output
 threads = snakemake.threads
+print(output)
 if len(files) == 1:
-    subprocess.run(["mv", files[0], output])
+    subprocess.run(["mv", files[0], output[0]])
 else:
-    subprocess.run(["sambamba", "-t", threads, output].extend(files))
+    subprocess.run(["sambamba", "-t", threads, output[0]].extend(files))
